7bf92b07238d39163fd85f1a064488f48d27096b
MarkR91 <mramdahin@yahoo.com>
2023-07-22 17:32:31 -0400

    Update to decode_message_file_1.py

--- DecodeMessageFromFile/decode_message_file_1.py
+++ DecodeMessageFromFile/decode_message_file_1.py
@@ -1,29 +1,37 @@
 #Functions:
+def decode(message_file):
 
-def triangle(list):
+  list =[] #Declare list variable as Python technically dosen't use arrays.
 
+  for x in message_file:
+    ln = x.split(" ")
+    list.append(ln)
+
+  list.sort()
+  output(list)
+
+
+def output(list):
+  
   n=0
   m=1
   i=2
 
   for x in list:
     print(list[n:m])
+    print(list[m-1][1])
     n=m
     m=m+i
-    i=i+1
+    i=i+1  
     if(list[n:m]==[]):
       break
+   
+#Beginning of program
 
-# Beginning of program
-f = open("input.txt","r") #Open text file
+message_file= open("file.txt","r") #Opens text file assuming its in the same folder as the Python program file.
+decode(message_file)
 
-list =[] #Declare list variable as Python technically dosen't use arrays
+message_file.close()
 
-for x in f:
-   ln = x.split(" ")
-   list.append(ln)
 
-list.sort()
-triangle(list)
 
-f.close()
