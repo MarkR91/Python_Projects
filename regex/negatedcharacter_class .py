6fe7c54e8448301eef0ae9d8248b57e2d8997c08
MarkR91 <mramdahin@yahoo.com>
2023-10-30 18:31:41 -0400

    negated character regex expression

--- regex/negatedcharacter_class .py
+++ regex/negatedcharacter_class .py
@@ -1,16 +1,15 @@
-"""import re
-
-lines = ["Hello world", "island", "Another line", "important info"]
-
-pattern = re.compile(r"^[^i]*$")
-
-for line in lines:
-    if pattern.match(line):
-        print(line)
 """
+Use negated character class regex:
+
+^[^i]*$
+```
+
+"""
+
+
 import re
 
-lines = ["Hello", "ifdf line", "Another line", "Start"]
+lines = ["Hello", "if", "Another line", "Start"]
 
 pattern = re.compile(r"^[^i]*$")
 
